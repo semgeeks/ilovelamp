a00181bf6a6261ed8c5bdce87d9948e8202ff5b0
Charles Lorenzo <charles@semgeeks.com>
2016-04-26 17:24:15 -0400

    backup script bug fixes

--- utilities/db_backup.sh
+++ utilities/db_backup.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 backup_parent_dir="/var/www/scheduled/backups"
-website_root_dir="/var/www/html/ROOT_HERE"
-site_name="ROOT_HERE"
-dropbox_client_name="api_test"
-dropbox_api_key=""
+website_root_dir="/var/www/html/NAME_HERE"
+site_name="NAME_HERE"
+dropbox_client_name="DROPBOX_CLIENT_FOLDER_NAME"
+dropbox_api_key="API_KEY"
 
 # MySQL settings
-mysql_user="root"
-mysql_password=""
+mysql_user="USERNAME"
+mysql_password="PASSWORD"
 
 # Read MySQL password from stdin if empty
 if [ -z "${mysql_password}" ]; then
@@ -50,13 +52,14 @@ done
 # Backup and compress site files
 tar -zcvf "${backup_dir}/${site_name}_site_files.tar.gz" "${website_root_dir}"
 chmod 600 "${backup_dir}/${site_name}_site_files.tar.gz"
-
 # Compress DB and site files
-tar -zcvf "${backup_parent_dir}/${site_name}-${backup_date}.tar.gz" "${backup_dir}"
+tar -zcvf "${site_name}-${backup_date}.tar.gz" "${backup_dir}"
 
 # Send to compressed backup directory to dropbox
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-  --header 'Authorization: Bearer "${dropbox_api_key}"' \
-  --header 'Content-Type: application/octet-stream' \
-  --header 'Dropbox-API-Arg: {"path":"/semgeeks clients/${dropbox_client_name}/Web/Backups/${site_name}-${backup_date}.gz"' \
-  --data-binary @'${backup_dir}/${site_name}-${backup_date}.gz'
+uploadURL="https://content.dropboxapi.com/2/files/upload"
+header1="Authorization: Bearer ${dropbox_api_key}"
+header2="Content-Type: application/octet-stream"
+header3="Dropbox-API-Arg: {\"path\":\"/semgeeks clients/${dropbox_client_name}/Web/Backups/${site_name}-${backup_date}.tar.gz\", \"mode\": \"add\"}"
+file="@${site_name}-${backup_date}.tar.gz"
+
+curl -X "POST" --header "${header1}" --header "${header2}" --header "${header3}" --data-binary "${file}" ${uploadURL}
